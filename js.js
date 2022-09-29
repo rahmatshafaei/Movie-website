3d871ee111d943d35893652a92a2688392d03613
rahmatshafaei <84686755+rahmatshafaei@users.noreply.github.com>
2022-09-29 09:54:49 +0200

    Update js.js

--- js.js
+++ js.js
@@ -24,3 +24,44 @@ const tagsEl = document.getElementById('tags');
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const current = document.getElementById('current')
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    lastUrl = url;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results)
+            if (data.results.length !== 0) {
+                showMovies(data.results);
+                currentPage = data.page;
+                nextPage = currentPage + 1;
+                prevPage = currentPage - 1;
+                totalPages = data.total_pages;
+
+                current.innerText = currentPage;
+
+                if (currentPage <= 1) {
+                    prev
+                        .classList
+                        .add('disabled');
+                    next
+                        .classList
+                        .remove('disabled')
+                } else if (currentPage >= totalPages) {
+                    prev
+                        .classList
+                        .remove('disabled');
+                    next
+                        .classList
+                        .add('disabled')
+                } else {
+                    prev
+                        .classList
+                        .remove('disabled');
+                    next
+                        .classList
+                        .remove('disabled')
+                }
