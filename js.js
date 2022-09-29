0e9c2a050447c30f2f2543c7673a4f0f88a444d5
rahmatshafaei <84686755+rahmatshafaei@users.noreply.github.com>
2022-09-29 10:30:03 +0200

    r

--- js.js
+++ js.js
@@ -7,61 +7,18 @@ const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
 
 
-/*$.get(API_URL, function (data) {
-    $("#title").text(data.results[0].original_title);
-    let imgg = "https://image.tmdb.org/t/p/w500" + data
-        .results[0]
-        .poster_path;
-    $("#poster").attr('src', imgg);
+$.get(API_URL, function (data) {
+    console.log(data.results)
+    data.results.forEach(i => {
+        console.log(i.title)
+        var title = `<h1>${i.title}</h1>`
+        var kort ="<h1>${title}</h1>"
+        $("#kort").append(title)
+        
+    });
+    // $("#title").text(data.results[0].original_title);
+    // let imgg = "https://image.tmdb.org/t/p/w500" + data
+    //     .results[0]
+    //     .poster_path;
+    // $("#poster").attr('src', imgg);
 });
-*/
-const genres = []
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const tagsEl = document.getElementById('tags');
-
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const current = document.getElementById('current')
-
-
-getMovies(API_URL);
-
-function getMovies(url) {
-    lastUrl = url;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.results)
-            if (data.results.length !== 0) {
-                showMovies(data.results);
-                currentPage = data.page;
-                nextPage = currentPage + 1;
-                prevPage = currentPage - 1;
-                totalPages = data.total_pages;
-
-                current.innerText = currentPage;
-
-                if (currentPage <= 1) {
-                    prev
-                        .classList
-                        .add('disabled');
-                    next
-                        .classList
-                        .remove('disabled')
-                } else if (currentPage >= totalPages) {
-                    prev
-                        .classList
-                        .remove('disabled');
-                    next
-                        .classList
-                        .add('disabled')
-                } else {
-                    prev
-                        .classList
-                        .remove('disabled');
-                    next
-                        .classList
-                        .remove('disabled')
-                }
